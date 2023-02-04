036cbfd8f6db0b5e42df246fdbc23d59d97a85c4
Jared McMahan <jaredm788@gmail.com>
2023-02-04 13:36:54 -0700

    Backend Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
+  # final repo backend test 
   # Add your routes here
   get "/" do
     { message: "Good luck with your project!" }.to_json
