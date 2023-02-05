5fdb696c3615c769e3e404087771917f8cfbde44
Jared McMahan <jaredm788@gmail.com>
2023-02-05 11:20:15 -0700

    migrate teachers table

--- db/migrate/20230205173146_create_teachers.rb
+++ db/migrate/20230205173146_create_teachers.rb
@@ -1,4 +1,11 @@
 class CreateTeachers < ActiveRecord::Migration[6.1]
   def change
+    create_table :teachers do |t|
+      t.string :name
+      t.string :instrument
+      t.boolean :availability
+      t.integer :rate
+      t.timestamps
+    end
   end
 end
