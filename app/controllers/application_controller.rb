88d707e0d8f2196b2ac960d0a2f0a0bdf26534cf
Jared McMahan <jaredm788@gmail.com>
2023-02-05 10:38:27 -0700

    teachers migration create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,12 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   # final repo backend test 
   # Add your routes here
-  get "/" do
+  get "/tests" do
     { message: "Good luck with your project!" }.to_json
   end
+
+  # get '/tests' do
+  #   "Hello World"
+  # end
 
 end
