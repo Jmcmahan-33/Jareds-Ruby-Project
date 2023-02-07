b8580006f6cddb145f66f88b2f2ab2bcda842615
Jared McMahan <jaredm788@gmail.com>
2023-02-07 08:58:47 -0700

    migrate students table

--- db/migrate/20230207153911_create_students.rb
+++ db/migrate/20230207153911_create_students.rb
@@ -1,4 +1,11 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
+    create_table :students do |t|
+      t.string :name
+      t.string :instrument
+      t.string :comment
+      t.integer :teacher_id
+      t.timestamps
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_05_173146) do
+ActiveRecord::Schema.define(version: 2023_02_07_153911) do
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "instrument"
+    t.string "comment"
+    t.integer "teacher_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "teachers", force: :cascade do |t|
     t.string "name"
