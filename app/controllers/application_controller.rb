4bfd21d400e42fd74766fc2589b06874532af29f
Jared McMahan <jaredm788@gmail.com>
2023-02-16 06:11:43 -0700

    Comments decision

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,56 +1,10 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-
-
-  # COMMENTS CRUD
-    # READ 
-  get '/comments' do
-    # get all the teachers from the database
-    comments = Comment.all
-    # return a JSON response with an array of all the comments data
-    comments.to_json
-  end
-    # CREATE
-  post '/comments' do
-    student = Student.find_by(id: params[:id])
-    comment = Comment.create(body: params[:body], student_id: params[:student_id], teacher_id: params[:teacher_id])
-    comment.to_json
-  end
-
-
-
-  # UPDATE
-  patch '/comments/:id' do
-    # find the comment using the ID
-    comment = Comment.find(params[:id])
-
-    # update the comment in the database
-    comment.update(
-      body: params[:body],
-      student_id: params[:student_id],
-      teacher_id: params[:teacher_id],
-    )
-
-    # send back the updated comment as JSON
-    comment.to_json
-  end
-
-
-   # DELETE 
-   delete "/comments/:id" do
-    comment = Comment.find(params[:id])
-    comment.destroy
-    comment.to_json
-  end
-
-
   # TEACHERS CRUD
    #  READ 
   get '/teachers' do
-    # get all the teachers from the database
     teachers = Teacher.all
-    # return a JSON response with an array of all the teachers data
     teachers.to_json
   end
     # CREATE
@@ -58,13 +12,12 @@ class ApplicationController < Sinatra::Base
     teacher = Teacher.create(name: params[:name], instrument: params[:instrument], availability: params[:availability], rate: params[:rate])
     teacher.to_json
   end
+  
 
   # STUDENTS CRUD
     # READ
   get '/students' do
-    # get all the students from the database
     students = Student.all
-    # return a JSON response with an array of all the students data
     students.to_json
   end
 
@@ -73,4 +26,44 @@ class ApplicationController < Sinatra::Base
     student = Student.create(name: params[:name], instrument: params[:instrument], teacher_id: params[:teacher_id])
     student.to_json
   end
+
+
 end
+
+
+
+
+  # COMMENTS CRUD
+    # READ 
+  # get '/comments' do
+  #   # get all the teachers from the database
+  #   comments = Comment.all
+  #   # return a JSON response with an array of all the comments data
+  #   comments.to_json
+  # end
+  #   # CREATE
+  # post '/comments' do
+  #   student = Student.find_by(id: params[:id])
+  #   comment = Comment.create(body: params[:body], student_id: params[:student_id], teacher_id: params[:teacher_id])
+  #   comment.to_json
+  # end
+  # # UPDATE
+  # patch '/comments/:id' do
+  #   # find the comment using the ID
+  #   comment = Comment.find(params[:id])
+
+  #   # update the comment in the database
+  #   comment.update(
+  #     body: params[:body],
+  #     student_id: params[:student_id],
+  #     teacher_id: params[:teacher_id],
+  #   )
+  #   # send back the updated comment as JSON
+  #   comment.to_json
+  # end
+  #  # DELETE 
+  #  delete "/comments/:id" do
+  #   comment = Comment.find(params[:id])
+  #   comment.destroy
+  #   comment.to_json
+  # end
