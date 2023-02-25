4b01e15b82f1e5cf4e9755ccb043241a8420fbf2
Jared McMahan <jaredm788@gmail.com>
2023-02-25 11:03:21 -0700

    comment table drop

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_21_170120) do
-
-  create_table "comments", force: :cascade do |t|
-    t.string "body"
-    t.integer "student_id"
-    t.integer "teacher_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2023_02_25_022250) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
