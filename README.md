2f11f5309e0cf8eba7cddfacc1b6374de7ca40fc
Jared McMahan <jaredm788@gmail.com>
2023-02-26 10:22:39 -0700

    Cleanup

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 ## Description
 
-The J-Rock Music Lessons is an App that allows students to sign up for music lessons. During sign up the student can pick which teacher they want to conduct lessons with. Fortunately, our teachers are multi talented Rock musicians, so if the student changes their mind on what Intrument they want to play, they have the ability to do so while keeping the same teacher for the Semester. Since the school is a ever growing business, the app has the ability to hire new teachers. Everytime a student signs up, they will have the ability to pick the  hired teachers. Lastly, if the student wants to cancel lessons, they can go ahead and delete themselves from the student page. 
+The J-Rock Music Lessons is an App that allows students to sign up for music lessons. During sign up the student can pick which teacher they want to conduct lessons with. Fortunately, our teachers are multi-talented Rock musicians, so if the student changes their mind on what Instrument they want to play, they have the ability to do so while keeping the same teacher for the Semester. Since the school is an ever growing business, the app has the ability to hire new teachers. Everytime a student signs up, they will have the ability to pick the  hired teachers. Lastly, if the student wants to cancel lessons, they can go ahead and delete themselves from the student page.
 
 ## Installation
 In the terminal create a new directory with new file name. Once you are in the file of the new directory, you can fork and clone from the following: https://github.com/learn-co-curriculum/phase-3-sinatra-react-project/fork.
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,60 +1,36 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # before table drop 
-  # TEACHERS CRUD
-   #  READ 
-
-  #  possible include method for teachers.to_json
   get '/teachers' do
     teachers = Teacher.all
     teachers.to_json(include: :students)
   end
-    # CREATE
+
   post '/teachers' do
     teacher = Teacher.create(name: params[:name], musical_talents: params[:musical_talents], rate: params[:rate])
     teacher.to_json(include: :students)
   end
 
-  get '/teachers/:id' do
-    # look up the teacher in the database using its ID
-    teacher = Teacher.find(params[:id])
-    # send a JSON-formatted response of the teacher data
-    teacher.to_json(include: :students)
-  end
-
-
-  # STUDENTS CRUD
-    # READ
+  
   get '/students' do
     students = Student.all
     students.to_json(include: :teacher)
   end
-    # CREATE
-    # possible refactor, talk to Enoch about post
-    # create a student through teacher 
-      # finding a teacher through the teacher
-      # then go with line 31, the create
-      # make a variable equal to find_by teacher id. 
   
   post '/students' do
     student = Student.create(name: params[:name], instrument: params[:instrument], teacher_id: params[:teacher_id])
     student.to_json(include: :teacher)
   end
 
- 
-    # UPDATE 
-      # Possible update of the instrument. 
       patch '/students/:id' do
         student = Student.find(params[:id])
         student.update(
           name: params[:name],
           instrument: params[:instrument],
-         
         )
         student.to_json(include: :teacher)
       end
-    # DELETE 
+   
      delete "/students/:id" do
     student = Student.find(params[:id])
     student.destroy
